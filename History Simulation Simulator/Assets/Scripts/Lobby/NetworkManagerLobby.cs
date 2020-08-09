06b983662c7b3790c00d878c8abd4a4dd3cde9a8
Emi Brown <emicb55@gmail.com>
2020-08-09 14:36:59 -0500

    Basic Waiting Room

--- History Simulation Simulator/Assets/Scripts/Lobby/NetworkManagerLobby.cs
+++ History Simulation Simulator/Assets/Scripts/Lobby/NetworkManagerLobby.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,13 +7,19 @@ using Mirror;
 
 namespace EmiCB.Lobby {
     public class NetworkManagerLobby : NetworkManager {
+        [SerializeField] private int minPlayers = 1;
+
         [Scene] [SerializeField] private string menuScene = string.Empty;
 
         [Header("Room")]
         [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
 
+        // custom events
         public static event Action OnClientConnected;
         public static event Action OnClientDisconnected;
+
+        // list of waiting players
+        public List<NetworkRoomPlayerLobby> roomPlayers {get;} = new List<NetworkRoomPlayerLobby>();
 
         // load gameobjects from Resources/SpawnablePrefabs folder (Server)
         public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
@@ -53,11 +59,50 @@ namespace EmiCB.Lobby {
             /* */
         }
 
+        // when player joins, spawn them in and link with connection
         public override void OnServerAddPlayer(NetworkConnection conn) {
-            if (SceneManager.GetActiveScene().name == menuScene) {
+            if (SceneManager.GetActiveScene().path == menuScene) {
+                // check if player is host
+                bool isHost = roomPlayers.Count == 0;
+                // spawn player in
                 NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+                roomPlayerInstance.IsHost = isHost;
                 NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
             }
+        }
+
+        // when player leaves
+        public override void OnServerDisconnect(NetworkConnection conn) {
+            // remove player from list
+            if (conn.identity != null) {
+                var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+                roomPlayers.Remove(player);
+                //NotifyPlayersOfReadyState();
+            }
+
+            base.OnClientDisconnect(conn);
+        }
+
+        // reset player list when server is stopped
+        public override void OnStopServer() {
+            roomPlayers.Clear();
+        }
+
+        // loop through all players and update ready statuses
+        public void NotifyPlayersOfReadyState() {
+            foreach (var player in roomPlayers) {
+                player.HandleReadyToStart(IsReadyToStart());
+            }
+        }
+
+        private bool IsReadyToStart() {
+            if (numPlayers < minPlayers) return false;
+
+            foreach (var player in roomPlayers) {
+                if (!player.isReady) return false;
+            }
+
+            return true;
         }
     }
 }
--- History Simulation Simulator/Assets/Scripts/Lobby/NetworkRoomPlayerLobby.cs
+++ History Simulation Simulator/Assets/Scripts/Lobby/NetworkRoomPlayerLobby.cs
@@ -1,11 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
+using TMPro;
 
 namespace EmiCB.Lobby {
     public class NetworkRoomPlayerLobby : NetworkBehaviour {
-        
+        [Header("UI")]
+        [SerializeField] private GameObject lobbyUI = null;
+        [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+        [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+        [SerializeField] private Button startGameButton = null;
+
+        // server validate display name changes and ready status changes
+        [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+        public string displayName = "Loading...";
+        [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+        public bool isReady = false;
+
+        private bool isHost;
+        public bool IsHost {
+            set {
+                isHost = value;
+                startGameButton.gameObject.SetActive(value);
+            }
+        }
+
+        private NetworkManagerLobby room;
+        private NetworkManagerLobby Room {
+            get {
+                if (room != null) return room;
+                return room = NetworkManager.singleton as NetworkManagerLobby;
+            }
+        }
+
+        //
+        public override void OnStartAuthority() {
+            CmdSetDisplayName(PlayerNameInput.displayName);
+            lobbyUI.SetActive(true);
+        }
+
+        // keep player list updated
+        public override void OnStartClient() {
+            Room.roomPlayers.Add(this);
+            UpdateDisplay();
+        }
+        public override void OnNetworkDestroy() {
+            Room.roomPlayers.Remove(this);
+            UpdateDisplay();
+        }
+
+        public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDisplay();
+        public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+
+        // update the UI
+        private void UpdateDisplay() {
+            // find player that belongs to us
+            if (!hasAuthority) {
+                foreach (var player in Room.roomPlayers) {
+                    if (player.hasAuthority) {
+                        player.UpdateDisplay();
+                        break;
+                    }
+                }
+                return;
+            }
+
+            // clear all player texts
+            for (int i = 0; i < playerNameTexts.Length; i++) {
+                playerNameTexts[i].text = "Waiting For Player...";
+                playerReadyTexts[i].text = string.Empty;
+            }
+
+            // update all player texts
+            for (int i = 0; i < Room.roomPlayers.Count; i++) {
+                playerNameTexts[i].text = Room.roomPlayers[i].displayName;
+                playerReadyTexts[i].text = Room.roomPlayers[i].isReady ? "<color=green>Ready</color>" : "<color=red>Not Ready</color>";
+            }
+        }
+
+        // allow host to start game when everyone is ready
+        public void HandleReadyToStart(bool isReadyToStart) {
+            if (!isHost) return;
+            startGameButton.interactable = isReadyToStart;
+        }
+
+
+        // validate with server
+        [Command]
+        private void CmdSetDisplayName(string displayName) {
+            // add extra validation checks here (ex. banned names)
+            this.displayName = displayName;
+        }
+
+        [Command]
+        public void CmdReadyUp() {
+            isReady = !isReady;
+            Room.NotifyPlayersOfReadyState();
+        }
+
+        [Command]
+        public void CmdStartGame() {
+            if (Room.roomPlayers[0].connectionToClient != connectionToClient) return;
+            // TODO: start game
+        }
     }
 }
 
